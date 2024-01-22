fdf68c1b69201fce79aa5600ae5deed9139691e1
Sean Lacey <seanlacey2164@gmail.com>
2024-01-21 20:13:38 -0500

    set default window value for testing

--- cell.py
+++ cell.py
@@ -1,7 +1,7 @@
 from graphics import Point, Line
 
 class Cell():
-  def __init__(self, win):
+  def __init__(self, win=None):
     self._win = win
     self._x1 = None
     self._y1 = None
@@ -18,21 +18,22 @@ class Cell():
     self._x2 = x2
     self._y2 = y2
 
-    if self.has_left_wall:
-      left_wall_line = Line(Point(x1, y1), Point(x1, y2))
-      self._win.draw_line(left_wall_line)
+    if self._win:
+      if self.has_left_wall:
+        left_wall_line = Line(Point(x1, y1), Point(x1, y2))
+        self._win.draw_line(left_wall_line)
 
-    if self.has_right_wall:
-      right_wall_line = Line(Point(x2, y1), Point(x2, y2))
-      self._win.draw_line(right_wall_line)
+      if self.has_right_wall:
+        right_wall_line = Line(Point(x2, y1), Point(x2, y2))
+        self._win.draw_line(right_wall_line)
 
-    if self.has_top_wall:
-      top_wall_line = Line(Point(x1, y2), Point(x2, y2))
-      self._win.draw_line(top_wall_line)
-      
-    if self.has_bottom_wall:
-      bottom_wall_line = Line(Point(x1, y1), Point(x2, y1))
-      self._win.draw_line(bottom_wall_line)
+      if self.has_top_wall:
+        top_wall_line = Line(Point(x1, y2), Point(x2, y2))
+        self._win.draw_line(top_wall_line)
+        
+      if self.has_bottom_wall:
+        bottom_wall_line = Line(Point(x1, y1), Point(x2, y1))
+        self._win.draw_line(bottom_wall_line)
 
   def draw_move(self, to_cell, undo=False):
     curr_width = self._x2 - self._x1
--- maze.py
+++ maze.py
@@ -10,7 +10,7 @@ class Maze():
       num_cols,
       cell_size_x,
       cell_size_y,
-      win,
+      win=None,
     ):
     self._x1 = x1
     self._y1 = y1
@@ -31,7 +31,8 @@ class Maze():
 
     for i in range(self._num_cols):
       for j in range(self._num_rows):
-        self._draw_cell(i, j)
+        if self._win:
+          self._draw_cell(i, j)
 
   def _draw_cell(self, i, j):
     x1 = self._x1 + (i * self._cell_size_x)
@@ -39,6 +40,7 @@ class Maze():
     x2 = x1 + self._cell_size_x
     y2 = y1 + self._cell_size_y
     self._cells[i][j].draw(x1, y1, x2, y2)
+    self._animate()
 
   def _animate(self):
     self._win.redraw()
