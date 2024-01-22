56732cc5ef4008c24c460cb1c3a8fc71e6722380
Sean Lacey <seanlacey2164@gmail.com>
2024-01-21 19:08:48 -0500

    add functionality to draw move lines

--- cell.py
+++ cell.py
@@ -33,3 +33,19 @@ class Cell():
     if self.has_bottom_wall:
       bottom_wall_line = Line(Point(x1, y1), Point(x2, y1))
       self._win.draw_line(bottom_wall_line)
+
+  def draw_move(self, to_cell, undo=False):
+    curr_width = self._x2 - self._x1
+    curr_height = self._y2 - self._y1
+    next_width = to_cell._x2 - to_cell._x1
+    next_height = to_cell._y2 - to_cell._y1
+
+    curr_center = Point(self._x1 + curr_width / 2, self._y1 + curr_height / 2)
+    next_center = Point(to_cell._x1 + next_width / 2, to_cell._y1 + next_height / 2)
+
+    move_line = Line(curr_center, next_center)
+
+    if undo:
+      self._win.draw_line(move_line, fill_color="gray")
+    else:
+      self._win.draw_line(move_line, fill_color="red")
