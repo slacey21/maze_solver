a91dc7c69b58802adf07c221189a09cc9aad975b
Sean Lacey <seanlacey2164@gmail.com>
2024-02-03 15:17:30 -0500

    Implement maze solver - use recursive BFS algorithm to solve maze once created - change line color for moves to differ from cell color - add functions to draw line from start and to end before and after solving

--- maze.py
+++ maze.py
@@ -1,7 +1,7 @@
 from cell import Cell
-import time
+from graphics import Point, Line
 import random
-
+import time
 class Maze():
   def __init__(
       self,
@@ -57,6 +57,7 @@ class Maze():
     if self._win is None:
       return
     self._win.redraw()
+    time.sleep(.02)
 
   def _break_entrance_and_exit(self):
     self._cells[0][0].has_top_wall = False
@@ -103,5 +104,107 @@ class Maze():
         self._cells[new_i][new_j].has_bottom_wall = False
 
       self._break_walls_r(new_i, new_j)
+
+  def _draw_solved_maze_start(self):
+    if self._win is None:
+      return
+
+    start_point_x = self._cells[0][0]._x1 + (self._cells[0][0]._x2 - self._cells[0][0]._x1) / 2
+    start_point_y = self._cells[0][0]._y1
+    end_point_x = self._cells[0][0]._x1 + (self._cells[0][0]._x2 - self._cells[0][0]._x1) / 2
+    end_point_y = self._cells[0][0]._y1 + (self._cells[0][0]._y2 - self._cells[0][0]._y1) / 2
+    
+    start_point = Point(start_point_x, start_point_y)
+    end_point = Point(end_point_x, end_point_y)
+
+    start_line = Line(start_point, end_point)
+
+    self._win.draw_line(start_line, fill_color="blue")
+
+  def _draw_solved_maze_end(self):
+    if self._win is None:
+      return
+
+    start_point_x = self._cells[-1][-1]._x1 + (self._cells[0][0]._x2 - self._cells[0][0]._x1) / 2
+    start_point_y = self._cells[-1][-1]._y1 + (self._cells[-1][-1]._y2 - self._cells[-1][-1]._y1) / 2
+    end_point_x = self._cells[-1][-1]._x1 + (self._cells[-1][-1]._x2 - self._cells[-1][-1]._x1) / 2
+    end_point_y = self._cells[-1][-1]._y2 
+    
+    start_point = Point(start_point_x, start_point_y)
+    end_point = Point(end_point_x, end_point_y)
+
+    start_line = Line(start_point, end_point)
+
+    self._win.draw_line(start_line, fill_color="blue")
+
+  def solve(self):
+    self._draw_solved_maze_start()
+    self._solve_r(0, 0)
+    self._draw_solved_maze_end()
+
+  def _solve_r(self, i, j):
+    self._animate()
+    self._cells[i][j].visited = True
+    
+    if i == self._num_cols - 1  and j == self._num_rows - 1:
+      return True
+
+    # check cell to left
+    if (
+      i > 0
+      and not self._cells[i][j].has_left_wall
+      and not self._cells[i - 1][j].visited
+      ):
+      
+      self._cells[i][j].draw_move(self._cells[i - 1][j])
+
+      if self._solve_r(i - 1, j):
+        return True
+      else:
+        self._cells[i][j].draw_move(self._cells[i - 1][j], True)
+
+    # check cell to right
+    if (
+      i < self._num_cols - 1
+      and not self._cells[i][j].has_right_wall
+      and not self._cells[i + 1][j].visited
+      ):
+      
+      self._cells[i][j].draw_move(self._cells[i + 1][j])
+
+      if self._solve_r(i + 1, j):
+        return True
+      else:
+        self._cells[i][j].draw_move(self._cells[i + 1][j], True)
+
+    # check cell above
+    if (
+      j > 0
+      and not self._cells[i][j].has_top_wall
+      and not self._cells[i][j - 1].visited
+      ):
+      
+      self._cells[i][j].draw_move(self._cells[i][j - 1])
+
+      if self._solve_r(i, j - 1):
+        return True
+      else:
+        self._cells[i][j].draw_move(self._cells[i][j - 1], True)
+
+    # check cell below
+    if (
+      j < self._num_rows - 1
+      and not self._cells[i][j].has_bottom_wall
+      and not self._cells[i][j + 1].visited
+      ):
+      
+      self._cells[i][j].draw_move(self._cells[i][j + 1])
+
+      if self._solve_r(i, j + 1):
+        return True
+      else:
+        self._cells[i][j].draw_move(self._cells[i][j + 1], undo=True)
+    
+    return False
 
 
